f025d22ef63e621a819cbcacaf917298eb52f282
michelMarques122 <mm6321987mm@gmail.com>
2023-04-19 03:05:06 +0900

    Project's Initial Commit

---
+++ www/components/loader.js
@@ -0,0 +1,16 @@
+const professores = professores
+switch(professores){
+
+    case '0' :
+      console.log(' Graciete selecionada ')
+        break;
+    case '1' :
+      console.log(' José selecionado ')
+        break;
+    case '2' :
+      console.log(' Oswaldo selecionado ')
+        break;
+    case '3' :
+       console.log(' Jussimar selecionado ')
+        break;
+}
